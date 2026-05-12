a1c6d1169cd608dcb7df7014277bb00d07b57210
rhectoroc <rhectoroc@gmail.com>
2026-05-12 17:46:15 -0400

    chore: separate admin module and prepare for Easypanel deployment

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,11 @@
 import { Suspense, lazy } from 'react';
 import { Routes, Route } from 'react-router-dom';
-import { ProtectedRoute } from './components/ProtectedRoute';
 import { Loader2 } from 'lucide-react';
 
 // Lazy loaded pages
 const Home = lazy(() => import('./pages/Home').then(module => ({ default: module.Home })));
 const PropertyDetails = lazy(() => import('./pages/PropertyDetails').then(module => ({ default: module.PropertyDetails })));
 const Login = lazy(() => import('./pages/Login').then(module => ({ default: module.Login })));
-const AdminDashboard = lazy(() => import('./pages/admin/AdminDashboard').then(module => ({ default: module.AdminDashboard })));
-const CreateProperty = lazy(() => import('./pages/admin/CreateProperty').then(module => ({ default: module.CreateProperty })));
-const EditProperty = lazy(() => import('./pages/admin/EditProperty').then(module => ({ default: module.EditProperty })));
 const PrivacyPolicy = lazy(() => import('./pages/PrivacyPolicy').then(module => ({ default: module.PrivacyPolicy })));
 const TermsOfService = lazy(() => import('./pages/TermsOfService').then(module => ({ default: module.TermsOfService })));
 
@@ -24,30 +20,6 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/property/:id" element={<PropertyDetails />} />
         <Route path="/login" element={<Login />} />
-        <Route
-          path="/admin/dashboard"
-          element={
-            <ProtectedRoute>
-              <AdminDashboard />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/admin/create"
-          element={
-            <ProtectedRoute>
-              <CreateProperty />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/admin/edit/:id"
-          element={
-            <ProtectedRoute>
-              <EditProperty />
-            </ProtectedRoute>
-          }
-        />
         <Route path="/privacy" element={<PrivacyPolicy />} />
         <Route path="/terms" element={<TermsOfService />} />
       </Routes>
